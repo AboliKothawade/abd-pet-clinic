ed96c7f3004e9b879df32ea8bcd2d26844e1f0d5
AboliKothawade <abolikothawade1@gmail.com>
2025-03-15 12:07:58 +0530

    Initial Commit

---
+++ abd-pet-clinic/src/main/java/in/ak/main/AbdPetClinicApplication.java
@@ -0,0 +1,13 @@
+package in.ak.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbdPetClinicApplication.class, args);
+	}
+
+}
