b7845c358170b01287601dbc2309f439627a243e
AboliKothawade <abolikothawade1@gmail.com>
2025-03-15 12:20:08 +0530

    testcommit

--- abd-pet-clinic/src/main/java/in/ak/main/AbdPetClinicApplication.java
+++ abd-pet-clinic/src/main/java/in/ak/main/AbdPetClinicApplication.java
@@ -2,8 +2,10 @@ package in.ak.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("in.ak.main")
 public class AbdPetClinicApplication {
 
 	public static void main(String[] args) {
